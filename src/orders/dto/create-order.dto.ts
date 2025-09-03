241300c81cb63854220cc6735da2a18602a9c3ab
BenjiCastillo <castilloeguezbenji@gmail.com>
2025-09-03 11:56:36 -0400

    servicio de creacion de orders

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,30 +1,11 @@
-import {
-  IsNumber,
-  IsPositive,
-  IsEnum,
-  IsOptional,
-  IsBoolean,
-} from 'class-validator';
-import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+import { ValidateNested, IsArray, ArrayMinSize } from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalItems: number;
-
-  @IsEnum(OrderStatusList, {
-    message: `Invalid order status, the value must be ${OrderStatusList.join(
-      ', ',
-    )}`,
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING;
-
-  @IsOptional()
-  @IsBoolean()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
