f88867a5a113c65fed5d3b6c570d4bcecea94b5c
BenjiCastillo <castilloeguezbenji@gmail.com>
2025-09-30 10:29:50 -0400

    health check anadido

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common/exceptions/rpc-custom-exception.filter';
 
 async function bootstrap() {
@@ -9,7 +9,14 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -22,7 +29,8 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  logger.log(`Application is running on: http://localhost:${envs.port}`);
+  logger.log('Health check configured');
+  logger.log(`Gateway is running on: http://localhost:${envs.port}`);
 }
 bootstrap().catch((error) => {
   console.error(error);
